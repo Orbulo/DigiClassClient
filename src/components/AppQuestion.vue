88babf855b98af4152ea75b17088a99e08becc08
kevin qu <qukevin99@gmail.com>
2020-08-22 15:47:46 -0400

    appquestion

--- src/components/AppQuestion.vue
+++ src/components/AppQuestion.vue
@@ -1,20 +1,42 @@
 <template>
-  <div class="row" id="question">
-    <div class="col-2">
-      <div>
-        12 upvotes
+  <div class="container">
+    <div class="row" id="question">
+      <div id="voting">
+        <template>
+          <q-icon name="mdi-chevron-up" id="upvote" />
+        </template>
+        <q-field borderless id="votes" stack-label>
+          <template v-slot:control>
+            <div class="self-center full-width no-outline" tabindex="0">
+              <p style="font-size: 15px; margin-bottom: 0px">{{ upvotes }}</p>
+              <p style="font-size: 10px; margin-bottom: 0px">upvotes</p>
+            </div>
+          </template>
+        </q-field>
       </div>
-    </div>
-    <div class="col-10">
-      <a href="/questions"><h6>QUESTION TITLE HERE</h6></a>
-      <br />
-      <p>kalsdjflkasdjflksadjf</p>
+      <div class="col-11">
+        <a href="/questions"><h6>QUESTION TITLE HERE</h6></a>
+        <br />
+        <p>RANDOM CONTENT</p>
+        <p id="author">Written by: {{ author }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "AppQuestion",
+  data() {
+    return {
+      upvotes: ""
+    };
+  },
+  created() {
+    this.upvotes = 123;
+    this.author = "ABC";
+  }
+};
 </script>
 
 <style scoped>
@@ -25,5 +47,16 @@ h6 {
 #question {
   padding-top: 10px;
   padding-bottom: 10px;
+}
+#voting {
+  margin-right: 15px;
+  margin-left: 15px;
+  text-align: center;
+}
+#upvote {
+  font-size: 25px;
+}
+#author {
+  text-align: right;
 }
 </style>
