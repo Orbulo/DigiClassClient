7f33f082ce4850101a38e397e09a7bcd20970fd3
Leon Si <leonzalion@gmail.com>
2020-08-22 16:50:01 -0400

    mi

--- src/pages/Accounts/SignUp.vue
+++ src/pages/Accounts/SignUp.vue
@@ -1,61 +1,55 @@
 <template>
-  <div>
-    <div v-if="!onConfirm">
-      <h1>Sign Up</h1>
-      <input v-model="email" type="text" placeholder="Email" /><br />
-      <input v-model="password" type="text" placeholder="Password" /><br />
-      <button @click="submit">Submit</button>
-    </div>
-
-    <div v-if="onConfirm">
-      <h2>Confirm Sign Up</h2>
-      <p>
-        We have emailed you a verification code to confirm your account. Please
-        check your email and type in the verification code.
-      </p>
-      <input v-model="code" type="text" placeholder="Code" /><br />
-      <button @click="confirm">Submit</button>
-    </div>
-  </div>
+  <q-card
+    class='q-pa-md column items-center'
+  >
+    <h3 class='text-weight-bold'>Login</h3>
+    <q-form>
+      <q-input
+        label='Email'
+        class='q-mb-md'
+        v-model='email'
+        outlined
+      />
+      <q-input
+        label='Password'
+        type='password'
+        class='q-mb-md'
+        v-model='password'
+        outlined
+      >
+        <template v-slot:prepend>
+          <q-icon name='lock' />
+        </template>
+      </q-input>
+      <q-btn
+        class='q-my-md'
+        label='Register'
+        @click='onRegister'
+      />
+    </q-form>
+    <router-link to='/register'>
+      Don't have an account?
+    </router-link>
+  </q-card>
 </template>
 
 <script>
-import { Auth } from 'aws-amplify';
 export default {
   name: 'SignUp',
   data() {
     return {
-      password: '',
       email: '',
-      onConfirm: false,
-      code: ''
+      password: '',
     };
   },
   methods: {
-    submit() {
-      Auth.signUp({
-        username: this.email,
+    async onRegister() {
+      const { data } = await this.$axios.post('auth/register', {
+        email: this.email,
         password: this.password,
-        attributes: {
-          email: this.email
-        }
-      })
-        .then(data => {
-          console.log('Submit data:', data);
-          this.onConfirm = true;
-        })
-        .catch(err => console.log('Submit Error:', err));
-    },
-    confirm() {
-      // After retrieveing the confirmation code from the user
-      Auth.confirmSignUp(this.email, this.code, {
-        // Optional. Force user confirmation irrespective of existing alias. By default set to True.
-        forceAliasCreation: true
-      })
-        .then(data => {
-          this.$router.push('/login');
-        })
-        .catch(err => console.log('Confirm Error:', err));
+      });
+      const token = data.token;
+
     }
   }
 };
