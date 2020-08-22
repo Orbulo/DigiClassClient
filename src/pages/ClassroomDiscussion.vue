b65adba56cb95fff7af0f2260af8d21ad6a4df61
kevin qu <qukevin99@gmail.com>
2020-08-22 13:21:43 -0400

    discussion

--- src/pages/ClassroomDiscussion.vue
+++ src/pages/ClassroomDiscussion.vue
@@ -1,25 +1,40 @@
 <template>
-  <div class="q-pa-md row justify-center">
-    <div style="width: 100%; max-width: 400px">
+  <div class="q-pa-md row justify-center absolute-bottom">
+    <div style="width: 100%">
       <q-chat-message
-
+        name="me"
+        avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+        :text="['hey, how are you?']"
+        sent
+        stamp="7 minutes ago"
       />
+      <q-chat-message
+        name="Jane"
+        avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+        :text="[`doing fine, how r you?`]"
+        stamp="4 minutes ago"
+      />
+      <q-input filled v-model="newMessage" label="Enter a Message">
+        <template v-slot:append>
+          <q-icon name="send" />
+        </template>
+      </q-input>
     </div>
   </div>
 </template>
 
 <script>
+import { send } from "@quasar/extras/material-icons";
 export default {
-  name: 'ClassroomChat',
+  name: "ClassroomChat",
   data: () => ({
     chatMessages: [],
+    newMessage: ""
   }),
   created() {
-    this.$axios.get('')
+    this.$axios.get("");
   }
-}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
