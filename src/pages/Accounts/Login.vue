08af3e1a99379032298558b1341fdbf035351ad0
kevin qu <qukevin99@gmail.com>
2020-08-22 21:08:39 -0400

    asdfjkl

--- src/pages/Accounts/Login.vue
+++ src/pages/Accounts/Login.vue
@@ -1,45 +1,55 @@
 <template>
-  <div>
-    <div v-if="!signedIn">
+  <div class="items-center" style="height:100%; width:100%">
+    <div v-if="!signedIn" class="row items-center justify-center parent" style="height:100%">
       <!-- <amplify-authenticator /> -->
-      <input v-model="email" type="text" name="" placeholder="Email" />
-      <br />
-      <input
-        v-model="password"
-        type="password"
-        name=""
-        placeholder="Password"
-      />
-      <br />
-      <button @click="signIn">Sign In</button>
-      <br />
-      <button @click="signUp">Sign Up</button>
+      <q-form class="q-gutter-md q-my-auto">
+        <q-input
+          class="q-card"
+          filled
+          v-model="email"
+          type="text"
+          name
+          placeholder="Email"
+          lazy-rules
+          :rules="[ val => !validEmail('@') || 'Please enter a valid email']"
+        />
+        <q-input
+          class="q-card"
+          filled
+          v-model="password"
+          type="password"
+          name
+          placeholder="Password"
+        />
+        <q-btn class="q-card" color="primary" @click="signIn" label="Sign In" />
+        <q-btn class="q-card" color="primary" @click="signUp" label="Sign Out" />
+      </q-form>
     </div>
     <div v-if="signedIn">
-      <button @click="signOut">Sign Out</button>
+      <q-btn @click="signOut" label="Sign Out" style />
       <h3>Username: {{ user.username }}</h3>
     </div>
   </div>
 </template>
 
 <script>
-import { Auth } from 'aws-amplify';
-import { AmplifyEventBus } from 'aws-amplify-vue';
+import { Auth } from "aws-amplify";
+import { AmplifyEventBus } from "aws-amplify-vue";
 export default {
-  name: 'Login',
+  name: "Login",
   data() {
     return {
       signedIn: false,
       user: null,
-      email: '',
-      password: ''
+      email: "",
+      password: "",
     };
   },
   created() {
     this.findUser();
-    AmplifyEventBus.$on('authState', info => {
-      console.log('auth', info);
-      if (info === 'signedIn') {
+    AmplifyEventBus.$on("authState", (info) => {
+      console.log("auth", info);
+      if (info === "signedIn") {
         this.findUser();
       } else {
         this.signedIn = false;
@@ -47,44 +57,53 @@ export default {
     });
   },
   methods: {
+    validEmail(email) {
+      return email.includes("@");
+    },
     signIn() {
       Auth.signIn(this.email, this.password)
-        .then(user => {
+        .then((user) => {
           this.signedIn = !!user;
           this.user = user;
         })
-        .catch(err => console.log('signIn Error:', err));
+        .catch((err) => console.log("signIn Error:", err));
     },
     signOut() {
       Auth.signOut()
-        .then(data => {
+        .then((data) => {
           console.log(data);
           this.signedIn = !!data;
           this.user = null;
         })
-        .catch(err => console.log('signOut Error:', err));
+        .catch((err) => console.log("signOut Error:", err));
     },
     signUp() {
-      this.$router.push('/signup');
+      this.$router.push("/signup");
     },
     async findUser() {
       try {
         const user = await Auth.currentAuthenticatedUser();
         this.user = user;
         this.signedIn = true;
-        console.log('user', user);
-        const jwt = user
-          .getSignInUserSession()
-          .getIdToken()
-          .getJwtToken();
-        console.log('jwt', jwt);
+        console.log("user", user);
+        const jwt = user.getSignInUserSession().getIdToken().getJwtToken();
+        console.log("jwt", jwt);
       } catch (err) {
-        console.log('Error in findUser', err);
+        console.log("Error in findUser", err);
         this.signedIn = false;
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.q-card {
+  min-width: 400px;
+}
+.parent {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
